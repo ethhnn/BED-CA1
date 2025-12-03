480048035c4bd630653770adc49d9083b8ce2042
ethanchuwasixuan <ethanchuwa03.25@ichat.sp.edu.sg>
2025-12-03 12:13:51 +0800

    week7commit1

--- src/controllers/userCompletionController.js
+++ src/controllers/userCompletionController.js
@@ -1,6 +1,6 @@
 const userCompletionModel = require("../models/userCompletionModel.js");
 
 
-module.exports.anySuitableFunctionName = (req, res, next) =>{ // Endpoint number
+module.exports.anySuitableFunctionName = (req, res, next) =>{ // Endpoint number 
 
 }
